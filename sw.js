47a565bb8c8cddb9185d42aa7cb31867a34ff59c
Denis Sebastião Costa Barbosa <denis.barbosa@inatel.br>
2022-10-08 16:07:55 -0300

    display offline page when no internal connection

--- sw.js
+++ sw.js
@@ -1,14 +1,34 @@
-self.addEventListener('install', () => {
-    console.log('[Service Worker] Installing service worker')
-    self.skipWaiting();
+const staticCache = "app-shell-v1";
+
+const assetsToCache = ["offline.html"];
+
+async function cacheStaticAssets() {
+  const cache = await caches.open(staticCache);
+  return cache.addAll(assetsToCache);
+}
+
+async function networkFirst(request) {
+  try {
+    return await fetch(request);
+  } catch (error) {
+    console.log("[Service Worker] network error", error);
+    const cache = await caches.open(staticCache);
+    return cache.match('offline.html');
+  }
+}
+
+self.addEventListener("install", (event) => {
+  console.log("[Service Worker] Installing service worker");
+  event.waitUntil(cacheStaticAssets());
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', () => {
-    console.log('[Service Worker] Activating service worker!')
-    return self.clients.claim();
+self.addEventListener("activate", () => {
+  console.log("[Service Worker] Activating service worker!");
+  return self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) =>{
-    console.log('[Service Worker] Fetch event worker!', event.request);
-    event.respondWith(fetch(event.request.url));
-})
+self.addEventListener("fetch", (event) => {
+  console.log("[Service Worker] Fetch event worker!", event.request.url);
+  event.respondWith(networkFirst(event.request));
+});
